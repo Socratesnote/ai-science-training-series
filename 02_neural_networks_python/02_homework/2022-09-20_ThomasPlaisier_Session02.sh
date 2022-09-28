4a8b1e058b89b1f0d71f6b969d189982f0c3fd9a
Thomas Plaisier <soc@thetalogin6.tmi.alcf.anl.gov>
2022-09-28 22:52:34 +0000

    Added shell.

---
+++ 02_neural_networks_python/02_homework/2022-09-20_ThomasPlaisier_Session02.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+module load /lus/theta-fs0/software/datascience/conda/2021-09-22
+
+CWD="$(pwd)"
+
+/lus/theta-fs0/software/datascience/conda/2021-09-22/mconda3/bin/python 2022-09-20_ThomasPlaisier_Session02.py
