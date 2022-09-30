8c937040968559ff4cba429d7235cad3365af657
Thomas <38224599+Socratesnote@users.noreply.github.com>
2022-09-30 14:03:46 -0500

    Reformatted: moved all imports to top of file. Model trainer now also reports time it took to train model. Implemented model saving as a definition. Defined ranges for the grid search, and a simplified range to test first.

--- 02_neural_networks_python/02_homework/2022-09-27_ThomasPlaisier_Session02.py
+++ 02_neural_networks_python/02_homework/2022-09-27_ThomasPlaisier_Session02.py
@@ -8,17 +8,28 @@
 # Currently is scores about 40% on the testing data, so just try to get higher. No worries about scoring 99%.
 # To submit: just submit your version of the full_net notebook.
 
-# %% 
+# %%
 # General imports.
+from fc_net import TwoLayerNet
+import ssl
+import requests
+import pickle
+import matplotlib.pyplot as plt
+import numpy
+import tensorflow as tf
+from IPython import get_ipython
 import logging
 import common_py.combilogger as clogger
 import datetime
+import time
+from sklearn.model_selection import ParameterGrid
 
 # %%
 # Configure combilogger.
 dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
-log = clogger.Combilogger(logLevel=logging.DEBUG, logPath=".", fileName=dt_string, writeMode="w")
+log = clogger.Combilogger(logLevel=logging.DEBUG,
+                          logPath=".", fileName=dt_string, writeMode="w")
 log.set_format_console('%(message)s')
 log.set_format_file('%(asctime)s - %(levelname)s - %(message)s')
 log.set_level_console(logging.INFO)
@@ -27,7 +38,6 @@ log.info("2022-09-27_ThomasPlaisier_Session02")
 
 # %%
 # Special settings for running in interactive mode.
-from IPython import get_ipython
 ipython = get_ipython()
 
 if '__IPYTHON__' in globals():
@@ -37,15 +47,13 @@ if '__IPYTHON__' in globals():
 
 # %%
 # Imports.
-import tensorflow as tf
-import numpy
-import matplotlib.pyplot as plt
-import pickle
 
 # %%
 # Definitions.
 
 # A simple implementation of stochastic gradient descent.
+
+
 def sgd(model, gradients, learning_rate):
     for p, w in model.params.items():
         dw = gradients[p]
@@ -54,12 +62,16 @@ def sgd(model, gradients, learning_rate):
     return model
 
 # One training step.
+
+
 def learn(model, x_train, y_train_onehot, learning_rate):
     loss, gradients = model.loss(x_train, y_train_onehot)
     model = sgd(model, gradients, learning_rate)
     return loss, model
 
 # Measure of accuracy.
+
+
 def accuracy(model, x, true_values):
     scores = model.loss(x)
     predictions = numpy.argmax(scores, axis=1)
@@ -68,22 +80,29 @@ def accuracy(model, x, true_values):
     return acc
 
 # Model creator.
+
+
 def model_builder(num_features, hidden_layer_width, nb_classes, initial_weight_scale):
     # The weights are initialized from a normal distribution with standard deviation (weight_scale).
-    model = TwoLayerNet(input_dim=num_features, hidden_dim=hidden_layer_width, num_classes=nb_classes, weight_scale=initial_weight_scale)
+    model = TwoLayerNet(input_dim=num_features, hidden_dim=hidden_layer_width,
+                        num_classes=nb_classes, weight_scale=initial_weight_scale)
     log.debug("Model created.")
     return model
 
-# Here's an example training loop using this two-layer model. Can you do better? 
+# Model trainer.
+
+
 def model_trainer(model, x_train, y_train, y_train_onehot, num_epochs, batch_size, x_test, y_test):
     num_examples = x_train.shape[0]
     num_batches = int(num_examples / batch_size)
     # Pre-allocate.
     losses = numpy.zeros(num_batches*num_epochs,)
     indices = numpy.arange(num_examples)
-    i = 0 # Iteration.
+    i = 0  # Iteration.
     acc_prev = 0
     delta_acc = 0.1
+
+    start_time = time.time()
 
     for epoch in range(0, num_epochs):
         # In each epoch, we loop over all of the training examples.
@@ -93,18 +112,23 @@ def model_trainer(model, x_train, y_train, y_train_onehot, num_epochs, batch_siz
             offset = step * batch_size
             batch_range = range(offset, offset+batch_size)
             x_train_batch = x_train[batch_range, :]
-            y_train_batch = y_train_onehot[batch_range,:]
-            
+            y_train_batch = y_train_onehot[batch_range, :]
+
             # Perform one SGD step.
-            loss, model = learn(model, x_train_batch, y_train_batch, learning_rate)
+            loss, model = learn(model, x_train_batch,
+                                y_train_batch, learning_rate)
             losses[i] = loss
             i += 1
 
+        # After stepping through the entire set, determine accuracy.
         acc = accuracy(model, x_train, y_train)
-        log.debug("Epoch: %d. Loss: %.2f. Accuracy %.1f." % (epoch, loss, 100*acc))
-        
+        log.debug("Epoch: %d. Loss: %.2f. Accuracy %.1f." %
+                  (epoch, loss, 100*acc))
+
+        # When accuracy stalls, stop the training loop: no point in performing 50 epochs when 7 will get you most of the way there.
         if (acc - acc_prev < delta_acc) & (epoch > 5):
-            log.info("Breaking at epoch %d due to stalling accuracy gains (< %.1f)." % (epoch, delta_acc))
+            log.info("Breaking at epoch %d due to stalling accuracy gains (< %.1f)." % (
+                epoch, delta_acc))
             break
 
         # Store.
@@ -112,18 +136,34 @@ def model_trainer(model, x_train, y_train, y_train_onehot, num_epochs, batch_siz
 
         # Shuffle the data so that we get a new set of batches. Ensure that everything is shuffled the same way.
         numpy.random.shuffle(indices)
-        x_train = x_train[indices,:]
+        x_train = x_train[indices, :]
         y_train = y_train[indices]
-        y_train_onehot = y_train_onehot[indices,:]
+        y_train_onehot = y_train_onehot[indices, :]
 
+    end_time = time.time()
+    log.info('Model took %.1f s to train.' % (end_time - start_time))
+
+    # After model is trained, determine accuracy with the testing dataset.
     t_acc = accuracy(model, x_test, y_test)
     return t_acc, losses
 
+# Save model to file.
+
+
+def save_model(model):
+    dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    params = model.get_params()
+    file_name = "model_acc_%.1f_HLW_%i_WS_%.3f_LR_%.3f_BS_%.0f_ME_%i_%s.pkl" % (
+        100*t_acc, hidden_layer_width, initial_weight_scale, learning_rate, batch_size, max_epochs, dt_string)
+    file_handle = open(file_name, "wb")
+    pickle.dump(params, file_handle)
+    file_handle.close()
+    log.info("Model saved to '%s'." % (file_name))
+
+
 # %%
 # Fix to avoid invalid SSL certificates on Theta.
-import requests
 requests.packages.urllib3.disable_warnings()
-import ssl
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -138,106 +178,135 @@ else:
 
 # Load MNIST data from Keras.
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-log.info('MNIST data loaded. # Training elements: %i, # Testing elements: %i.' % (len(x_train), len(x_test)))
+log.info('MNIST data loaded. # Training elements: %i, # Testing elements: %i.' %
+         (len(x_train), len(x_test)))
 
 # %%
 # Data preparation
 
 # Convert from uint8 to float32.
 x_train = x_train.astype(numpy.float32)
-x_test  = x_test.astype(numpy.float32)
+x_test = x_test.astype(numpy.float32)
 
 # Normalize range from [0, 255] to [0, 1].
 x_train /= 255.
-x_test  /= 255.
+x_test /= 255.
 
 log.info('Input data is of shape: %s' % (list(x_train.shape)))
 # Reshape data from [N, d1, d2] to [N, d1*d2].
-x_train = x_train.reshape(x_train.shape[0], numpy.prod(x_train[0,:,:].shape))
-x_test = x_test.reshape(x_test.shape[0], numpy.prod(x_test[0,:,:].shape))
+x_train = x_train.reshape(x_train.shape[0], numpy.prod(x_train[0, :, :].shape))
+x_test = x_test.reshape(x_test.shape[0], numpy.prod(x_test[0, :, :].shape))
 log.info('Input data reshaped to: %s' % (list(x_train.shape)))
 
 # Convert from uint8 to int32.
 y_train = y_train.astype(numpy.int32)
-y_test  = y_test.astype(numpy.int32)
+y_test = y_test.astype(numpy.int32)
 log.info('Label data is of shape: %s' % (list(y_train.shape)))
 
 # Convert from real integers to one-hot encoding (categorical):
-num_features = x_train.shape[1] # Number of pixels
+num_features = x_train.shape[1]  # Number of pixels
 nb_classes = 10
 y_train_onehot = tf.keras.utils.to_categorical(y_train, nb_classes)
 y_test_onehot = tf.keras.utils.to_categorical(y_test, nb_classes)
 
 # %%
 # Import neural network.
-# This implementation is a two-layer neural network. Credit: Stanford's CSE231n course, 
+# This implementation is a two-layer neural network. Credit: Stanford's CSE231n course,
 # hosted at https://github.com/cs231n/cs231n.github.io with the MIT license.
-from fc_net import TwoLayerNet
 
 # %%
 # Hyperparameters
 # Number of units in hidden layers.
-hidden_layer_width = 300 # Default, 98.2
+hidden_layer_width = 300  # Default, 98.2
 # hidden_layer_width = 3000 # 98.2, but takes ages.
 # hidden_layer_width = 30 # 96.5
+hlw_range = [3, 30]
+# hlw_range = [3, 30, 100, 250, 300, 1000, 2000, 3000]
 
 # Standard divation of normal distribution of weights.
-initial_weight_scale = 0.01 # Default, 98.2
+initial_weight_scale = 0.01  # Default, 98.2
 # initial_weight_scale = 0.001 # 97.9
 # initial_weight_scale = 0.1 # 97.8
 # initial_weight_scale = 1.0 # 93.4
+# iws_range = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0]
+iws_range = [0.01, 0.05]
 
 # Learning rate of model.
 # learning_rate = 5.0 # 10.3
 # learning_rate = 1.0 # 97.7
 # learning_rate = 0.7 # 98
-learning_rate = 0.5 # 98.2
+learning_rate = 0.5  # 98.2
 # learning_rate = 0.2 # 97.5
 # learning_rate = 0.1 # % 96.6
 # learning_rate = 0.06 # 95.4%
 # learning_rate = 0.04 # 94.2%
 # learning_rate = 0.01 # Default
+# lr_range = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 5.0]
+lr_range = [0.1, 0.5]
 
 # Batch size for SGD.
-batch_size = 100 # Pretty good, 90% with the rest default.
+batch_size = 100  # Pretty good, 90% with the rest default.
 # batch_size = 10000 # Default
+# bs_range = [10, 50, 100, 500, 1000, 5000, 10000, 20000, 30000]
+bs_range = [100, 10000]
 
 # Number of epochs.
 # In most cases you don't need to go higher than 30. The loop will automatically break when stalling.
-num_epochs = 50
+max_epochs = 50
 # num_epochs_range = numpy.round(numpy.linspace(15, 52, 6)).astype(numpy.int32)
 
 # %%
-# Build, train, and test model.
+# Build a parameter grid.
+param_grid = {'HLW': hlw_range, 'IWS': iws_range,
+              'LR': lr_range, 'BS': bs_range}
 
-model = model_builder(num_features, hidden_layer_width, nb_classes, initial_weight_scale)
+# %%
+# Store all accuracies to find out which set was best.
+t_acc_all = []
+# %%
+# Build, train, and test model across all sets in the parameter grid.
 
-t_acc, losses = model_trainer(model, x_train, y_train, y_train_onehot, num_epochs, batch_size, x_test, y_test)
+for set in list(ParameterGrid(param_grid)):
+    hidden_layer_width = set['HLW']
+    initial_weight_scale = set['IWS']
+    learning_rate = set['LR']
+    batch_size = set['BS']
 
-log.info("Hyperparameters. HLW: %i. WS: %.3f. LR: %.3f. Batch: %.0f. Max epochs: %i." % (hidden_layer_width, initial_weight_scale, learning_rate, batch_size, num_epochs))
-log.info("Testing accuracy: %.1f." % (100*t_acc))
+    log.info("Hyperparameters. HLW: %i. WS: %.3f. LR: %.3f. BS: %.0f. ME: %i." % (
+        hidden_layer_width, initial_weight_scale, learning_rate, batch_size, max_epochs))
+    model = model_builder(num_features, hidden_layer_width,
+                          nb_classes, initial_weight_scale)
 
+    t_acc, losses = model_trainer(
+        model, x_train, y_train, y_train_onehot, max_epochs, batch_size, x_test, y_test)
+    log.info("Testing accuracy: %.1f." % (100*t_acc))
 
-# %% 
-# Show predictions for 10 random images.
+    save_model(model)
+    t_acc_all.append(t_acc)
+
+# %%
+# Show best model.
+log.warning("Best model:")
+best_acc_index = numpy.argmax(t_acc_all)
+log.warning("Testing accuracy: %.1f" % (100*t_acc_all[best_acc_index]))
+best_set = ParameterGrid(param_grid)[best_acc_index]
+log.warning("Model: %s" % (best_set))
+
+# %%
+# Show predictions for 10 random images for last model.
 indices = numpy.random.randint(0, x_train.shape[0], 10)
-x_disp = x_train[indices,:]
+x_disp = x_train[indices, :]
 scores = model.loss(x_disp)
 predictions = numpy.argmax(scores, axis=1)
 
 for i in range(10):
-    plt.subplot(1,10,i+1)
+    plt.subplot(1, 10, i+1)
     plt.axis('off')
-    plt.imshow(numpy.reshape(x_disp[i,:], (28, 28)), cmap="gray")
+    plt.imshow(numpy.reshape(x_disp[i, :], (28, 28)), cmap="gray")
     plt.title('%.0f' % predictions[i])
 
 # %%
-# Save model to file.
-dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-params = model.get_params()
-file_name = "model_acc_%.1f_HLW_%i_WS_%.3f_LR_%.3f_Batch_%.0f_Max epochs_%i_%s.pkl" % (t_acc, hidden_layer_width, initial_weight_scale, learning_rate, batch_size, num_epochs, dt_string)
-file_handle = open(file_name, "wb")
-pickle.dump(params, file_handle)
-file_handle.close()
+# Close the log file when done.
+del(log)
 
 # %%
