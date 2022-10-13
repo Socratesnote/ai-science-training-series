ab76abc8c555d4661b82b4fb0d3f826a0ccd1629
Thomas Plaisier <soc@thetalogin6.tmi.alcf.anl.gov>
2022-10-13 22:12:27 +0000

    Made things work. Too tired to type a commit message.

--- 04_modern_neural_networks/train_resnet34_self.py
+++ 04_modern_neural_networks/train_resnet34_self.py
@@ -9,6 +9,9 @@ os.environ["TF_XLA_FLAGS"] = "--tf_xla_auto_jit=2"
 
 import tensorflow as tf
 import glob
+import numpy
+import matplotlib.pyplot as plt
+import datetime
 
 # %%
 #########################################################################
@@ -37,7 +40,8 @@ class ResidualLayer(tf.keras.Model):
         self.norm2 = tf.keras.layers.BatchNormalization()
 
     def call(self, inputs):
-
+        
+        # Note that this is the same step as the self.identity stage of the residual downsample layer, but with kernel size (1,1) and stride (1,1) --> simple equality.
         x = inputs
 
         output1 = self.norm1(self.conv1(inputs))
@@ -46,6 +50,7 @@ class ResidualLayer(tf.keras.Model):
 
         output2 = self.norm2(self.conv2(output1))
 
+        # Note the residual here as "+ x".
         return tf.keras.activations.relu(output2 + x)
 
 #########################################################################
@@ -104,6 +109,7 @@ class ResNet34(tf.keras.Model):
     def __init__(self):
         super(ResNet34, self).__init__()
 
+        # Sequence of downsampling with conv2D, normalization, and maxpool.
         self.conv_init = tf.keras.Sequential([
             tf.keras.layers.Conv2D(
                 filters     = 64,
@@ -118,6 +124,7 @@ class ResNet34(tf.keras.Model):
 
         ])
 
+        # Sequence of 1:1 residuals.
         self.residual_series_1 = tf.keras.Sequential([
             ResidualLayer(64),
             ResidualLayer(64),
@@ -127,6 +134,7 @@ class ResNet34(tf.keras.Model):
         # Increase the number of filters:
         self.downsample_1 = ResidualDownsample(128)
 
+        # Sequence of 1:1 residuals.
         self.residual_series_2 = tf.keras.Sequential([
             ResidualLayer(128),
             ResidualLayer(128),
@@ -136,6 +144,7 @@ class ResNet34(tf.keras.Model):
         # Increase the number of filters:
         self.downsample_2 = ResidualDownsample(256)
 
+        # Sequence of 1:1 residuals.
         self.residual_series_3 = tf.keras.Sequential([
             ResidualLayer(256),
             ResidualLayer(256),
@@ -147,16 +156,18 @@ class ResNet34(tf.keras.Model):
         # Increase the number of filters:
         self.downsample_3 = ResidualDownsample(512)
 
-
+        # Once more unto the breach for 1:1.
         self.residual_series_4 = tf.keras.Sequential([
             ResidualLayer(512),
             ResidualLayer(512),
         ])
 
+        # Smoothing with an averaging layer.
         self.final_pool = tf.keras.layers.AveragePooling2D(
             pool_size=(8,8)
         )
 
+        # Finally, flatten and classify with a dense layer.
         self.flatten = tf.keras.layers.Flatten()
         self.classifier = tf.keras.layers.Dense(1000)
 
@@ -167,9 +178,7 @@ class ResNet34(tf.keras.Model):
 
         x = self.residual_series_1(x)
 
-
         x = self.downsample_1(x)
-
 
         x = self.residual_series_2(x)
 
@@ -179,112 +188,24 @@ class ResNet34(tf.keras.Model):
 
         x = self.downsample_3(x)
 
-
         x = self.residual_series_4(x)
-
 
         x = self.final_pool(x)
 
         x = self.flatten(x)
 
         logits = self.classifier(x)
-
+        
+        # Return results as logits (probabilities).
         return logits
 
 
 # %%
 
-@tf.function
-def calculate_accuracy(logits, labels):
-    # We calculate top1 accuracy only here:
-    selected_class = tf.argmax(logits, axis=1)
+# Define helper functions.
 
-    correct = tf.cast(selected_class, tf.float32) == tf.cast(labels, tf.float32)
-
-    return tf.reduce_mean(tf.cast(correct, tf.float32))
-
-
-@tf.function
-def calculate_loss(logits, labels):
-    # Note that _with_logits is chosen, otherwise your network won't converge.
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
-    return tf.reduce_mean(loss)
-
-@tf.function
-def training_step(network, optimizer, images, labels):
-    with tf.GradientTape() as tape:
-        logits = network(images)
-        loss = calculate_loss(logits, labels)
-
-    gradients = tape.gradient(loss, network.trainable_variables)
-
-    optimizer.apply_gradients(zip(gradients, network.trainable_variables))
-
-    accuracy = calculate_accuracy(logits, labels)
-
-    return loss, accuracy
-
-def train_epoch(i_epoch, step_in_epoch, train_ds, val_ds, network, optimizer, BATCH_SIZE, checkpoint):
-    # Here is our training loop!
-
-    steps_per_epoch = int(1281167 / BATCH_SIZE)
-    steps_validation = int(50000 / BATCH_SIZE)
-
-    start = time.time()
-    for train_images, train_labels in train_ds.take(steps_per_epoch):
-        if step_in_epoch > steps_per_epoch: break
-        else: step_in_epoch.assign_add(1)
-
-        # Peform the training step for this batch
-        loss, acc = training_step(network, optimizer, train_images, train_labels)
-        end = time.time()
-        images_per_second = BATCH_SIZE / (end - start)
-        # Convenient sanity check: if this line fails to compile, you forgot to activate the conda 2022-07-01 environment (and python3).
-        print(f'Finished step {step_in_epoch.numpy()} of {steps_per_epoch} in epoch {i_epoch.numpy()}, loss={loss:.3f}, acc={acc:.3f} ({images_per_second:.3f} img/s).')
-        start = time.time()
-
-    # Save the network after every epoch:
-    checkpoint.save("resnet34/model")
-
-    # Compute the validation accuracy:
-    mean_accuracy = None
-    for val_images, val_labels in val_ds.take(steps_validation):
-        logits = network(val_images)
-        accuracy = calculate_accuracy(logits, val_labels)
-        if mean_accuracy is None:
-            mean_accuracy = accuracy
-        else:
-            mean_accuracy += accuracy
-
-    mean_accuracy /= steps_validation
-
-    print(f"Validation accuracy after epoch {i_epoch.numpy()}: {mean_accuracy:.4f}.")
-
-
-
-def prepare_data_loader(BATCH_SIZE):
-
-
-    tf.config.threading.set_inter_op_parallelism_threads(8)
-    tf.config.threading.set_intra_op_parallelism_threads(8)
-
-    print("Parameters set, preparing dataloading")
-    #########################################################################
-    # Here's the part where we load datasets:
-    import json
-
-
-    # What's in this function?  Tune in next week ...
-    from ilsvrc_dataset import get_datasets
-
-
-    class FakeHvd:
-
-        def size(self): return 1
-
-        def rank(self): return 0
-
-    # Add this to find the ilsvrc file from session 4.
+# Define a search function for the .json configuration.
+def get_config_file():
     print('Looking for ilsvrc.json.')
     json_files = glob.glob("**/ilsvrc.json", recursive=True) 
     if len(json_files) > 1:
@@ -304,16 +225,39 @@ def prepare_data_loader(BATCH_SIZE):
     this_file = this_file[0]
     print('Using file:')
     print(this_file)
+    return this_file
+
+# Define a data loader.
+def prepare_data_loader(BATCH_SIZE):
+
+    # Define number of threads to use for parallel loading: at most 8.
+    tf.config.threading.set_inter_op_parallelism_threads(8)
+    tf.config.threading.set_intra_op_parallelism_threads(8)
+
+    print("Parameters set, preparing dataloading")
+    #########################################################################
+    # Here's the part where we load datasets:
+    import json
+    # What's in this function?  Tune in next week ...
+    from ilsvrc_dataset import get_datasets
+
+    # What is this supposed to fake?
+    class FakeHvd:
+
+        def size(self): return 1
+
+        def rank(self): return 0
+
+    # Add this to find the ilsvrc file from session 4.
+    this_file = get_config_file()
 
     with open(this_file, 'r') as f:
         config = json.load(f)
 
     print(json.dumps(config, indent=4))
 
-
     config['hvd'] = FakeHvd()
     config['data']['batch_size'] = BATCH_SIZE
-    # Seems like the config is missing the data paths.
 
     train_ds, val_ds = get_datasets(config)
     print("Datasets ready, creating network.")
@@ -321,52 +265,228 @@ def prepare_data_loader(BATCH_SIZE):
 
     return train_ds, val_ds
 
+# Accuracy of logits versus true labels.
+@tf.function
+def calculate_accuracy(logits, labels):
+    # We calculate top1 accuracy (one-hot encoding) only here:
+    selected_class = tf.argmax(logits, axis=1)
+
+    # Comparing this as floats seems a bit weird, but OK.
+    correct = tf.cast(selected_class, tf.float32) == tf.cast(labels, tf.float32)
+
+    # reduce_mean just calculates the mean of a tensor.
+    mean_accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+    return mean_accuracy
+
+# Loss of logits versus labels.
+@tf.function
+def calculate_loss(logits, labels):
+    # Note that _with_logits is chosen, otherwise the network won't converge.
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits)
+    # Return average loss.
+    mean_loss = tf.reduce_mean(tf.cast(loss, tf.float32))
+    return mean_loss
+
+# Define one training step.
+@tf.function
+def training_step(network, optimizer, images, labels):
+    with tf.GradientTape() as tape:
+        logits = network(images)
+        loss = calculate_loss(logits, labels)
+
+    gradients = tape.gradient(loss, network.trainable_variables)
+
+    optimizer.apply_gradients(zip(gradients, network.trainable_variables))
+
+    accuracy = calculate_accuracy(logits, labels)
+
+    return loss, accuracy
+
+# Define validation accuracy calculation.
+# Why can't this be turned into a function? seems to cause issues with initializing mean_accuracy=None.
+@tf.function
+def validate_model(network, val_ds, steps_validation):
+    mean_accuracy = None
+    for val_images, val_labels in val_ds.take(steps_validation):
+        logits = network(val_images)
+        accuracy = calculate_accuracy(logits, val_labels)
+        print(accuracy)
+        if mean_accuracy is None:
+            mean_accuracy = accuracy
+        else:
+            mean_accuracy += accuracy
+
+    mean_accuracy /= steps_validation
+    return mean_accuracy
+
+# Define one training epoch.
+def train_epoch(i_epoch, step_in_epoch, train_ds, val_ds, network, optimizer, BATCH_SIZE, checkpoint, acc_history, loss_history):
+    # Calculate the steps per epoch and validation loop based on the known dataset sizes. Why not get these from the *_ds inputs?
+    l_train = tf.data.experimental.cardinality(train_ds)
+    l_val = tf.data.experimental.cardinality(val_ds)
+    steps_training = int(1281167 / BATCH_SIZE)
+    steps_validation = int(50000 / BATCH_SIZE)
+    i_step = 0
+
+    # Step over the training dataset with steps_per_epoch.
+    for train_images, train_labels in train_ds.take(steps_training):
+        start = time.time()
+        if step_in_epoch > steps_training: break
+        # Use assign_add because step is a reference for some reason.
+        else: step_in_epoch.assign_add(1)
+
+        # Peform the training step for this batch.
+        loss, acc = training_step(network, optimizer, train_images, train_labels)
+
+        acc_history[i_epoch, i_step] = acc
+        loss_history[i_epoch, i_step] = loss
+        i_step += 1
+
+        end = time.time()
+        images_per_second = BATCH_SIZE / (end - start)
+        # Convenient sanity check: if this line fails to compile, you forgot to activate the conda 2022-07-01 environment (and python3).
+        
+        # Save progress every so often.
+        if step_in_epoch % 500 == 0:
+            print(f'Checkpointed model at step {step_in_epoch.numpy()}.')
+            checkpoint.save("resnet34/model")
+
+        # Print progress every so often.
+        if step_in_epoch % 100 == 0:
+            print(f'Finished step {step_in_epoch.numpy()} of {steps_training} in epoch {i_epoch.numpy()}, loss={loss:.3f}, acc={acc:.3f} ({images_per_second:.3f} img/s).')
+            # Temp
+            # print('Temporary shortcut.')
+            # return acc_history, loss_history
+
+    # Save the network after every epoch:
+    checkpoint.save("resnet34/model")
+
+    # Compute the validation accuracy:
+    # mean_accuracy = validate_model(network, val_ds, steps_validation)
+    mean_accuracy = None
+    for val_images, val_labels in val_ds.take(steps_validation):
+        logits = network(val_images)
+        accuracy = calculate_accuracy(logits, val_labels)
+        print(accuracy)
+        if mean_accuracy is None:
+            mean_accuracy = accuracy
+        else:
+            mean_accuracy += accuracy
+
+    mean_accuracy /= steps_validation
+
+    print(f"Validation accuracy after epoch {i_epoch.numpy()}: {mean_accuracy:.4f}.")
+    return acc_history, loss_history
+
+def plot_figure(i_epoch,loss_history, acc_history):
+    dt_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    # Plot.
+    print('Loss history')
+    print(loss_history)
+    print('Acc history')
+    print(acc_history)
+
+    plt.subplot(211)
+    plt.plot(loss_history[i_epoch,:], color='blue')
+    plt.title(f'Epoch {i_epoch.numpy()}: Loss')
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss Value")
+    # plot accuracy
+    plt.subplot(212)
+    plt.plot(100*acc_history[i_epoch,:], color='blue')
+    plt.title(f'Epoch {i_epoch.numpy()}: Classification Accuracy')
+    plt.xlabel("Iterations")
+    plt.ylabel("Accuracy %")
+    # save plot to file
+    plt.savefig(f'{dt_string}_epoch_{i_epoch.numpy()}_resnet34_plot.png')
+    plt.close()
+    print(f'Saved figure {dt_string}_epoch_{i_epoch.numpy()}_resnet34_plot.png.')
+
 # %%
 def main():
     #########################################################################
     # Here's some configuration:
     #########################################################################
     BATCH_SIZE = 256
-    N_EPOCHS = 1
+    # BATCH_SIZE = 1024 # Causes OOM.
+    N_EPOCHS = 3
 
+    # Get datasets.
     train_ds, val_ds = prepare_data_loader(BATCH_SIZE)
 
-
+    # Show an example image.
     example_images, example_labels = next(iter(train_ds.take(1)))
-
-
     print("Initial Image size: ", example_images.shape)
+
+    # Initialize network.
     network = ResNet34()
-
     output = network(example_images)
-    print("output shape:", output.shape)
+    print("Output shape:", output.shape)
 
+    # Print network parameters.
     print(network.summary())
 
+    # Define epoch and steps as state variables: this allows all TF operations on these variables..
     epoch = tf.Variable(initial_value=tf.constant(0, dtype=tf.dtypes.int64), name='epoch')
     step_in_epoch = tf.Variable(
         initial_value=tf.constant(0, dtype=tf.dtypes.int64),
         name='step_in_epoch')
 
+    # We need an optimizer. Let's use Adam:
+    learning_rate = 0.0001
+    print(f'Optimizing with ADAM, LR = {learning_rate:.5f}.')
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-    # We need an optimizer.  Let's use Adam:
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-
+    # Define a checkpoint manager.
     checkpoint = tf.train.Checkpoint(
         network       = network,
         optimizer     = optimizer,
         epoch         = epoch,
         step_in_epoch = step_in_epoch)
 
+    steps_training = int(1281167 / BATCH_SIZE)
+    steps_validation = int(50000 / BATCH_SIZE)
+
     # Restore the model, if possible:
     latest_checkpoint = tf.train.latest_checkpoint("resnet34/")
     if latest_checkpoint:
+        print('Restoring checkpoint.')
         checkpoint.restore(latest_checkpoint)
+        print('Checking accuracy on load.')
+        # Compute the validation accuracy:
+        l_val = tf.data.experimental.cardinality(val_ds)
+        l_train = tf.data.experimental.cardinality(train_ds)
+        # print(f'Val steps: {steps_validation:d}, train steps: {steps_training:d}')
+        # mean_accuracy = validate_model(network, val_ds, steps_validation)
+        mean_accuracy = None
+        for val_images, val_labels in val_ds.take(steps_validation):
+            logits = network(val_images)
+            accuracy = calculate_accuracy(logits, val_labels)
+            if mean_accuracy is None:
+                mean_accuracy = accuracy
+            else:
+                mean_accuracy += accuracy
+
+        mean_accuracy /= steps_validation
+
+        print(f"Validation accuracy on load (epoch {epoch.numpy()}, step {step_in_epoch.numpy()}): {mean_accuracy:.4f}.")
+
+
+    acc_history = numpy.zeros((N_EPOCHS, steps_training))
+    loss_history = numpy.zeros((N_EPOCHS, steps_training))
 
     while epoch < N_EPOCHS:
-        train_epoch(epoch, step_in_epoch, train_ds, val_ds, network, optimizer, BATCH_SIZE, checkpoint)
+        acc_history, loss_history = train_epoch(epoch, step_in_epoch, train_ds, val_ds, network, optimizer, BATCH_SIZE, checkpoint, acc_history, loss_history)
+
+        plot_figure(epoch, loss_history, acc_history)
+
         epoch.assign_add(1)
         step_in_epoch.assign(0)
+
+    
+    # Save after all epochs all done.
+    checkpoint.save("resnet34/model")
 
 # %%
 # Actually run.
--- 04_modern_neural_networks/train_resnet34_thetagpu.sh
+++ 04_modern_neural_networks/train_resnet34_thetagpu.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -l
-#COBALT -t 120
 #COBALT -q full-node
-#COBALT -A ALCFAITP
+#COBALT -t 120
 #COBALT -n 1
+#COBALT -A ALCFAITP
+#COBALT -M tp@u.northwestern.edu
 #COBALT --attrs filesystems=home,grand
+
+# Make executable with 
+# chmod +x train_resnet34_thetagpu.sh
+
+# Submit with
+# qsub-gpu train_resnet34_thetagpu.sh
 
 # Data is stored on Grand for the purposes of this class.
 
@@ -20,5 +27,9 @@ python train_resnet34_self.py
 
 # Instead of through the interactive 'notebook', this has to run in interactive terminal mode with
 # qsub-gpu -q single-gpu -t 60 -I -n 1 -A ALCFAITP --attrs filesystems=home,grand
-# Or use the Jupyter environment.
+# or
+# qsub-gpu -q full-node -t 60 -I -n 1 -A ALCFAITP --attrs filesystems=home,grand
+# module load conda/2022-07-01
+# conda activate
+# clear; python ./ai-science-training-series/04_modern_neural_networks/train_resnet34_self.py
 # Note that on the interactive thetaGPU, you need to enter both 'module load conda/2022-07-01' AND 'conda activate' to set the environment.
