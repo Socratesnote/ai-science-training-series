4bb434d49ed2cb6ddce1d78917390c1fa3f4695b
Thomas Plaisier <soc@thetalogin6.tmi.alcf.anl.gov>
2022-10-25 20:17:47 +0000

    Made session 6 shell script in my usual format.

--- 06_distributedTraining/train_resnet34_thetagpu.sh
+++ 06_distributedTraining/train_resnet34_thetagpu.sh
@@ -1,17 +1,27 @@
 #!/bin/bash -l
-#COBALT -t 60
 #COBALT -q full-node
+#COBALT -t 60
 #COBALT -n 1
-#COBALT -A datascience
-##COBALT -A ALCFAITP
+#COBALT -A ALCFAITP
+#COBALT -M tp@u.northwestern.edu
+#COBALT --attrs filesystems=home,grand
 
-#  #########3#COBALT --attrs filesystems=home:eagle
+# Make executable with 
+# chmod +x train_resnet34_thetagpu.sh
+
+# Submit with
+# qsub-gpu train_resnet34_thetagpu.sh
+
+# qsub-gpu -q single-gpu -t 60 -I -n 1 -A ALCFAITP --attrs filesystems=home,grand
+# or
+# qsub-gpu -q full-node -t 120 -I -n 1 -A ALCFAITP --attrs filesystems=home,grand
 
 # Set up software deps:
 module load conda/2022-07-01
 conda activate
 
 # You have to point this to YOUR local copy of ai-science-training-series
+cd /home/soc/ai-science-training-series/06_distributedTraining/
 
 export TF_XLA_FLAGS="--tf_xla_auto_jit=2"
 mpirun -np 8 python train_resnet34_hvd.py --num_steps 1000000
